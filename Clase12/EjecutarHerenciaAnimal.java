0427e99b6d42f071e4af999ad7538b03b4af8b52
josepadilla-pixel <jose.padillacal@campusucc.edu.co>
2025-10-22 20:49:01 +0000

    herencia java

---
+++ Clase12/EjecutarHerenciaAnimal.java
@@ -0,0 +1,21 @@
+package Clase12;
+
+public class EjecutarHerenciaAnimal {
+    public static void main(String[] args) {
+        
+        Animal tomoe = new gato(); 
+        Animal doki = new perro();
+
+       tomoe.hacersonido();
+       doki.hacersonido();
+
+
+
+    }
+
+
+
+
+
+
+}
